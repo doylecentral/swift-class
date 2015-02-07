3299f1aa57ca5d2eee636bc7d67c545a2a82b485
doyle <doylecentral@gmail.com>
2015-02-07 11:42:32 -0600

    Some Clean up. Whammy on the gravity for the world.

--- iPong-TWO/GameScene.swift
+++ iPong-TWO/GameScene.swift
@@ -11,101 +11,60 @@ import SpriteKit
 
 struct PhysicsCategory {
     static let None      : UInt32 = 0
-    static let All       : UInt32 = UInt32.max
-    static let Monster   : UInt32 = 0b1       // 1
-    static let Projectile: UInt32 = 0b10      // 2
+    //    static let All       : UInt32 = UInt32.max
+    //    static let Monster   : UInt32 = 0b1       // 1
+    //    static let Projectile: UInt32 = 0b10      // 2
 }
 
 class GameScene: SKScene , SKPhysicsContactDelegate{
     
-   
-
-
-
-    let velocity: CGFloat = 3
-    let radius : CGFloat = 40
+    
+    func setPhysics(){
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: UIScreen.mainScreen().bounds)
+        self.physicsBody?.friction = 0
+        //self.physicsWorld.gravity.dy = 0.0
+        //self.physicsWorld.gravity.dx = 0.0
+//                 self.physicsWorld.gravity = CGVectorMake(0.0, 0)
+        self.backgroundColor = UIColor.blackColor()
+        println("Yes Was Called")
+        
+        var ball = addBall()
+        ball.physicsBody?.applyImpulse(CGVectorMake(0, 10))
+        self.addChild(ball)
+        
+    }
+    
     
     override func didMoveToView(view: SKView) {
-//       self.physicsBody = SKPhysicsBody(ed: self.frame)
-        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: UIScreen.mainScreen().bounds)
-        self.physicsWorld.gravity = CGVectorMake(0, 0);
         
+        /*
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
+        */
         
-        //physicsWorld.gravity = CGVectorMake(0, 0)
-        //physicsWorld.contactDelegate = self
+        physicsWorld.contactDelegate = self
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        addBall()
-        
-//        
-//        for touch: AnyObject in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
-        
+        //Just leaving it in here
         for touch: AnyObject in touches {
             println("Touch?")
         }
         
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
         //moveBall()
-
-    }
-    
-    func moveBall(){
-        /*
-        Need to move this
-        */
-        var bounds: CGRect = UIScreen.mainScreen().bounds
-        var width:CGFloat = bounds.size.width
-        var height:CGFloat = bounds.size.height
-        
-        
-        // (ball_pos[1] <= BALL_RADIUS) or (ball_pos[1] >= HEIGHT - BALL_RADIUS):
-        //#then flip the direction of the y vector
-        //ball_vel[1] = -ball_vel[1]
-/*
-        
-        if(circle.position.x >= (width - radius)){
-            println("FLIP:")
-            circle.position.x = circle.position.x - velocity
-        }else{
-            circle.position.x = circle.position.x + velocity //CGPointMake( circle.position.x + velocity , circle.position.y)
-        }
-  */
-//
-//        if(circle.position.x <= (width - radius))
-//        {
-//            
-//            circle.position = CGPointMake(circle.position.x + velocity , circle.position.y)
-//        }
-  //      println("Circle Position: \(circle.position)")
         
     }
     
-    func addBall(){
+    
+    func  addBall() -> SKShapeNode{
         var circle = SKShapeNode(circleOfRadius: 40)
         //var circle = SKSpriteNode(color: UIColor.whiteColor(), size: CGSizeMake(10, 10))
         
@@ -113,43 +72,16 @@ class GameScene: SKScene , SKPhysicsContactDelegate{
         circle.name = "defaultCircle"
         circle.strokeColor = SKColor.blackColor()
         circle.fillColor = SKColor.whiteColor()
-        circle.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(radius))
+        circle.physicsBody = SKPhysicsBody(circleOfRadius: CGFloat(40))
         circle.physicsBody?.dynamic = true
-        circle.physicsBody?.friction = 0
-        circle.physicsBody?.linearDamping = 0
+        circle.physicsBody?.friction = 0.0
+        circle.physicsBody?.affectedByGravity = true
+        circle.physicsBody?.linearDamping = 0.0
         circle.physicsBody?.restitution = 1.0
+//        circle.physicsBody?.mass = 0.0
+        
         circle.physicsBody?.allowsRotation = false
-        circle.physicsBody?.applyImpulse(CGVectorMake(10, -10))
-        
-        self.addChild(circle)
-        
+        //        circle.physicsBody?.applyImpulse(CGVectorMake(10, -10), atPoint: CGPointMake(200, 300))
+        return circle
     }
-    
-    /*
-    func addBall() {
-        
-        // Create sprite
-        let monster = SKSpriteNode(imageNamed: "monster")
-        
-        
-        // Determine where to spawn the monster along the Y axis
-        let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
-        
-        // Position the monster slightly off-screen along the right edge,
-        // and along a random position along the Y axis as calculated above
-        monster.position = CGPoint(x: size.width + monster.size.width/2, y: actualY)
-        
-        // Add the monster to the scene
-        addChild(monster)
-        
-        // Determine speed of the monster
-        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
-        
-        // Create the actions
-        let actionMove = SKAction.moveTo(CGPoint(x: -monster.size.width/2, y: actualY), duration: NSTimeInterval(actualDuration))
-        let actionMoveDone = SKAction.removeFromParent()
-        monster.runAction(SKAction.sequence([actionMove, actionMoveDone]))
-        
-    }
-*/
 }
--- iPong-TWO/GameViewController.swift
+++ iPong-TWO/GameViewController.swift
@@ -26,30 +26,39 @@ extension SKNode {
 }
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    
+    
+    
+    
+    
+    //    override func viewDidLoad() {
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        //        super.viewDidLoad()
+        
         if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
             // Configure the view.
             let skView = self.view as SKView
+            
             skView.showsFPS = true
             skView.showsNodeCount = true
-            
+
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
-            
+        
             /* Set the scale mode to scale to fit the window */
+
             scene.scaleMode = .AspectFill
             
+            scene.setPhysics()
             skView.presentScene(scene)
         }
     }
-
+    
     override func shouldAutorotate() -> Bool {
         return true
     }
-
+    
     override func supportedInterfaceOrientations() -> Int {
         if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
             return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
@@ -57,12 +66,12 @@ class GameViewController: UIViewController {
             return Int(UIInterfaceOrientationMask.All.rawValue)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
